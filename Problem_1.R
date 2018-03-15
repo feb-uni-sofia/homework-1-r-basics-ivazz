37a05c45b853808a7dd9678dc7005126169914fe
Boyko Amarov <boyko.amarov@gmail.com>
2018-03-15 21:10:44 +0200

    HW

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,12 @@
-
+x <- c(4, 1, 1, 4)
+y <- c(1, 4)
+x-y
+s <- c(x, y)
+rep (s, 10)
+z <- rep (s, 10)
+length(z)
+rep (s, each=3)
+seq (7, 21)
+7:21
+length(7:21)
+#HW
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,16 @@
-
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+xmin-xmax
+Avgxmin <- mean(xmin)
+Avgxmin
+Avgmax <- mean(xmax)
+Avgmax
+xmin[xmin<Avgxmin]
+xmin[xmax>Avgmax]
+names(xmin) <- MinTemp
+names(xmax) <- MaxTemp
+MaxTemp <- names(xmax)
+MinTemp <- names(xmin)
+Temperatures <- data.frame(xmin=MinTemp, xmax=MaxTemp)
+Temperatures <-within(Temperatures,{MinTempFahrenheit=MinTemp*9/5+32})
+##
